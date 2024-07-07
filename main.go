197e1ed7f6db11b7a0d211ac1c20adaad7ed6249
Lucas <lucas.rimfrost@gmail.com>
2024-07-07 19:23:22 +0000

    Get users first and last name

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter text: ")
-    input, _ := reader.ReadString('\n')
-    fmt.Println("You entered:", input)
+
+    fmt.Print("Enter your first name: ")
+    firstName, _ := reader.ReadString('\n')
+    firstName = strings.TrimSpace(firstName)
+
+    fmt.Print("Enter your last name: ")
+    lastName, _ := reader.ReadString('\n')
+    lastName = strings.TrimSpace(lastName)
+
+    fmt.Printf("You first name is: %s, and your last name is %s.\n", firstName, lastName)
 }
 
